23903508341a8523a2d2cc06d8f485cf62a67f05
jdickey <josh.dickey1@gmail.com>
2016-07-04 00:22:22 -0600

    Add numbers ListView and ArrayList

--- app/src/main/java/com/example/android/mymiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/mymiwok/NumbersActivity.java
@@ -2,6 +2,13 @@ package com.example.android.mymiwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +16,41 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+       /* LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        int count = 0;*/
+
+        /*while (count < words.size()){
+
+
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(count));
+            rootView.addView(wordView);
+
+            count++;
+        }*/
+
+
+
+
+
+
     }
 }
