1a6c10fc3e1dbcd8f753ac83a387939f2c78c8e8
jdickey <josh.dickey1@gmail.com>
2016-07-05 20:26:12 -0600

    Add numbers ListView, ArrayList, and list_items Activity

--- app/src/main/java/com/example/android/mymiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/mymiwok/NumbersActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static android.widget.GridView.*;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -30,7 +34,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Nine");
         words.add("Ten");
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
 
